f753e19707afa8cc6c6beb35fb68ab9f39074f72
Gleb Sinyavskiy <zhulik.gleb@gmail.com>
2025-01-08 00:37:24 +0100

    Move log package's content to di

--- internal/di/di.go
+++ internal/di/di.go
@@ -1,21 +1,41 @@
 package di
 
 import (
+	"fmt"
+
 	"github.com/samber/do"
+	"github.com/sirupsen/logrus"
 	"github.com/zhulik/fid/internal/backends"
 	"github.com/zhulik/fid/internal/config"
+	"github.com/zhulik/fid/internal/core"
 	"github.com/zhulik/fid/internal/forwarder"
 	"github.com/zhulik/fid/internal/gateway"
 	"github.com/zhulik/fid/internal/infoserver"
-	"github.com/zhulik/fid/internal/log"
 	"github.com/zhulik/fid/internal/pubsub"
 )
 
 func New() *do.Injector {
 	injector := do.New()
 
+	do.Provide[logrus.FieldLogger](injector, func(_ *do.Injector) (logrus.FieldLogger, error) {
+		cfg, err := do.Invoke[core.Config](injector)
+		if err != nil {
+			return nil, err
+		}
+
+		logger := logrus.New()
+
+		logLevel, err := logrus.ParseLevel(cfg.LogLevel())
+		if err != nil {
+			return nil, fmt.Errorf("failed parse loglevel: %w", err)
+		}
+
+		logger.SetLevel(logLevel)
+
+		return logger, nil
+	})
+
 	config.Register(injector)
-	log.Register(injector)
 
 	forwarder.Register(injector)
 	backends.Register(injector)
