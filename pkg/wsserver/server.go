eda7a19c77b2f93f5ae0e680bcb925d8554f9e35
Gleb Sinyavskiy <zhulik.gleb@gmail.com>
2025-01-07 22:40:35 +0100

    Use httpserver.Server in wsserver

--- pkg/wsserver/server.go
+++ pkg/wsserver/server.go
@@ -1,65 +1,52 @@
 package wsserver
 
 import (
-	"context"
 	"fmt"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
 	"github.com/gorilla/websocket"
 	"github.com/samber/do"
-	"github.com/sirupsen/logrus"
 	"github.com/zhulik/fid/pkg/core"
 	"github.com/zhulik/fid/pkg/httpserver"
 )
 
 type Server struct {
-	injector *do.Injector
-	backend  core.ContainerBackend
-	server   http.Server
-	error    error
+	*httpserver.Server
 
-	logger logrus.FieldLogger
+	backend core.ContainerBackend
 }
 
 // NewServer creates a new Server instance.
 func NewServer(injector *do.Injector) (*Server, error) {
-	logger, err := do.Invoke[logrus.FieldLogger](injector)
-	if err != nil {
-		return nil, err
-	}
-
-	logger = logger.WithField("component", "wsserver.Server")
-
-	defer logger.Info("Server created.")
-
-	router := httpserver.NewRouter(injector, logger)
-
 	config, err := do.Invoke[core.Config](injector)
 	if err != nil {
 		return nil, err
 	}
+
+	server, err := httpserver.NewServer(injector, "wsserver.Server", config.WSServerPort())
+	if err != nil {
+		return nil, fmt.Errorf("failed to create a new http server: %w", err)
+	}
+
+	logger := server.Logger()
+
+	defer logger.Info("Server created.")
 
 	backend, err := do.Invoke[core.ContainerBackend](injector)
 	if err != nil {
 		return nil, err
 	}
 
-	server := &Server{
-		injector: injector,
-		backend:  backend,
-		server: http.Server{
-			Addr:              fmt.Sprintf("0.0.0.0:%d", config.WSServerPort()),
-			ReadHeaderTimeout: httpserver.ReadHeaderTimeout,
-			Handler:           router,
-		},
-		logger: logger,
+	srv := &Server{
+		Server:  server,
+		backend: backend,
 	}
 
 	// TODO: authentication
-	router.GET("/ws/:functionName", server.WebsocketHandler)
+	srv.Router().GET("/ws/:functionName", srv.WebsocketHandler)
 
-	return server, nil
+	return srv, nil
 }
 
 func (s *Server) WebsocketHandler(c *gin.Context) {
@@ -73,7 +60,7 @@ func (s *Server) WebsocketHandler(c *gin.Context) {
 	//	return
 	//}
 
-	s.logger.Debug("Function '", functionName, "' handler connected, upgrading to websocket connection...")
+	s.Logger().Debug("Function '", functionName, "' handler connected, upgrading to websocket connection...")
 	// Upgrade the HTTP connection to a WebSocket connection
 	upgrader := websocket.Upgrader{
 		CheckOrigin: func(_ *http.Request) bool { return true },
@@ -84,36 +71,9 @@ func (s *Server) WebsocketHandler(c *gin.Context) {
 		c.Error(err)
 	}
 
-	s.logger.Debug("Function '", functionName, "' handler successfully upgraded to websocket connection")
+	s.Logger().Debug("Function '", functionName, "' handler successfully upgraded to websocket connection")
 
-	wsConn := NewWebsocketConnection(functionName, conn, s.logger)
+	wsConn := NewWebsocketConnection(functionName, conn, s.Logger())
 	// TODO: handle error?
 	go wsConn.Handle() //nolint:errcheck
-}
-
-func (s *Server) HealthCheck() error {
-	s.logger.Debug("Server health check.")
-
-	return s.error
-}
-
-func (s *Server) Shutdown() error {
-	s.logger.Debug("Server shutting down...")
-	defer s.logger.Debug("Server shot down.")
-
-	err := s.server.Shutdown(context.Background())
-	if err != nil {
-		return fmt.Errorf("failed to shut down the https server: %w", err)
-	}
-
-	return nil
-}
-
-// Run starts the HTTP server.
-func (s *Server) Run() error {
-	s.logger.Info("Starting server at: ", s.server.Addr)
-
-	s.error = s.server.ListenAndServe()
-
-	return s.error
 }
