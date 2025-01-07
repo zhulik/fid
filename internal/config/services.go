4b15f643eef602349b0b7430d0f16690c781781f
Gleb Sinyavskiy <zhulik.gleb@gmail.com>
2025-01-08 00:53:21 +0100

    Cleanup

--- internal/config/services.go
+++ internal/config/services.go
@@ -9,7 +9,6 @@ import (
 )
 
 func Register(injector *do.Injector) {
-	// Currently it tries to detect your backend. In the future it should use external config.
 	do.Provide(injector, func(_ *do.Injector) (core.Config, error) {
 		var cfg Config
 
