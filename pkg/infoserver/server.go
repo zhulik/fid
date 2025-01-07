acafcfbd99b6f16032859b85883a46b51ed10216
Gleb Sinyavskiy <zhulik.gleb@gmail.com>
2025-01-07 22:42:34 +0100

    Use httpserver.Server in infoserver

--- pkg/infoserver/server.go
+++ pkg/infoserver/server.go
@@ -1,63 +1,50 @@
 package infoserver
 
 import (
-	"context"
 	"fmt"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
 	"github.com/samber/do"
-	"github.com/sirupsen/logrus"
 	"github.com/zhulik/fid/pkg/core"
 	"github.com/zhulik/fid/pkg/httpserver"
 )
 
 type Server struct {
-	injector *do.Injector
-	backend  core.ContainerBackend
-	server   http.Server
-	error    error
+	*httpserver.Server
 
-	logger logrus.FieldLogger
+	backend core.ContainerBackend
 }
 
 // NewServer creates a new Server instance.
 func NewServer(injector *do.Injector) (*Server, error) {
-	logger, err := do.Invoke[logrus.FieldLogger](injector)
-	if err != nil {
-		return nil, err
-	}
-
-	logger = logger.WithField("component", "infoserver.Server")
-
-	defer logger.Info("Server created.")
-
-	router := httpserver.NewRouter(injector, logger)
-
 	config, err := do.Invoke[core.Config](injector)
 	if err != nil {
 		return nil, err
 	}
+
+	server, err := httpserver.NewServer(injector, "infoserver.Server", config.WSServerPort())
+	if err != nil {
+		return nil, fmt.Errorf("failed to create a new http server: %w", err)
+	}
+
+	logger := server.Logger()
+
+	defer logger.Info("Server created.")
 
 	backend, err := do.Invoke[core.ContainerBackend](injector)
 	if err != nil {
 		return nil, err
 	}
 
-	server := &Server{
-		injector: injector,
-		backend:  backend,
-		server: http.Server{
-			Addr:              fmt.Sprintf("0.0.0.0:%d", config.InfoServerPort()),
-			Handler:           router,
-			ReadHeaderTimeout: httpserver.ReadHeaderTimeout,
-		},
-		logger: logger,
+	srv := &Server{
+		Server:  server,
+		backend: backend,
 	}
 
-	router.GET("/info", server.InfoHandler)
+	server.Router().GET("/info", srv.InfoHandler)
 
-	return server, nil
+	return srv, nil
 }
 
 func (s *Server) InfoHandler(c *gin.Context) {
@@ -67,31 +54,4 @@ func (s *Server) InfoHandler(c *gin.Context) {
 	}
 
 	c.JSON(http.StatusOK, info)
-}
-
-func (s *Server) HealthCheck() error {
-	s.logger.Debug("Server health check.")
-
-	return s.error
-}
-
-func (s *Server) Shutdown() error {
-	s.logger.Info("Server shutting down...")
-	defer s.logger.Info("Server shot down.")
-
-	err := s.server.Shutdown(context.Background())
-	if err != nil {
-		return fmt.Errorf("failed to shut down the https server: %w", err)
-	}
-
-	return nil
-}
-
-// Run starts the HTTP server.
-func (s *Server) Run() error {
-	s.logger.Info("Starting server at: ", s.server.Addr)
-
-	s.error = s.server.ListenAndServe()
-
-	return s.error
 }
