d4d4a9b126eaa5ccbe0560ae5383f481c917f01a
Gleb Sinyavskiy <zhulik.gleb@gmail.com>
2025-01-11 22:31:47 +0100

    Fix naming in gateway server, correcly handle client disconnects

--- internal/gateway/server.go
+++ internal/gateway/server.go
@@ -1,6 +1,8 @@
 package gateway
 
 import (
+	"context"
+	"errors"
 	"fmt"
 	"io"
 
@@ -8,19 +10,19 @@ import (
 	"github.com/google/uuid"
 	"github.com/samber/do"
 	"github.com/sirupsen/logrus"
-	core2 "github.com/zhulik/fid/internal/core"
+	"github.com/zhulik/fid/internal/core"
 	"github.com/zhulik/fid/pkg/httpserver"
 )
 
 type Server struct {
 	*httpserver.Server
 
-	publisher core2.Publisher
+	publisher core.Publisher
 }
 
 // NewServer creates a new Server instance.
 func NewServer(injector *do.Injector) (*Server, error) {
-	config, err := do.Invoke[core2.Config](injector)
+	config, err := do.Invoke[core.Config](injector)
 	if err != nil {
 		return nil, err
 	}
@@ -30,7 +32,7 @@ func NewServer(injector *do.Injector) (*Server, error) {
 		return nil, fmt.Errorf("failed to create a new http server: %w", err)
 	}
 
-	publisher, err := do.Invoke[core2.Publisher](injector)
+	publisher, err := do.Invoke[core.Publisher](injector)
 	if err != nil {
 		return nil, err
 	}
@@ -47,6 +49,7 @@ func NewServer(injector *do.Injector) (*Server, error) {
 
 func (s *Server) InvokeHandler(c *gin.Context) {
 	ctx := c.Request.Context()
+
 	functionName := c.Param("functionName")
 	invocationUUID := uuid.New()
 
@@ -62,10 +65,16 @@ func (s *Server) InvokeHandler(c *gin.Context) {
 		return
 	}
 
-	subject := fmt.Sprintf("%s.%s", core2.InvokeSubjectBase, invocationUUID)
+	subject := fmt.Sprintf("%s.%s.%s", core.InvokeSubjectBase, functionName, invocationUUID)
 
 	response, err := s.publisher.PublishWaitReply(ctx, subject, body)
 	if err != nil {
+		if errors.Is(err, context.Canceled) {
+			s.Logger.Info("client disconnected while waiting for reply")
+
+			return
+		}
+
 		c.Error(err)
 
 		return
