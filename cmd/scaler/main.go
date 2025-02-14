3e9fa13bbdbe74c86e6394ff9ed714e1032dfb78
Gleb Sinyavskiy <zhulik.gleb@gmail.com>
2025-02-14 23:19:47 +0100

    Update go to 1.24.0

---
+++ cmd/scaler/main.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"errors"
+	"net/http"
+	"syscall"
+
+	"github.com/samber/do"
+	"github.com/sirupsen/logrus"
+	"github.com/zhulik/fid/internal/di"
+	"github.com/zhulik/fid/internal/infoserver"
+)
+
+func main() {
+	injector := di.New()
+
+	logger := do.MustInvoke[logrus.FieldLogger](injector).WithField("component", "main")
+
+	logger.Info("Starting...")
+
+	server := do.MustInvoke[*infoserver.Server](injector)
+
+	go func() {
+		err := server.Run()
+
+		if errors.Is(err, http.ErrServerClosed) {
+			return
+		}
+
+		logger.WithError(err).Fatal("Failed to run server")
+	}()
+
+	logger.Info("Running...")
+
+	err := injector.ShutdownOnSignals(syscall.SIGINT, syscall.SIGTERM)
+	if err != nil {
+		logger.WithError(err).Fatal("Failed to shutdown")
+	}
+}
--- go.mod
+++ go.mod
@@ -1,6 +1,6 @@
 module github.com/zhulik/fid
 
-go 1.23.4
+go 1.24.0
 
 require (
 	github.com/caarlos0/env/v11 v11.3.1
